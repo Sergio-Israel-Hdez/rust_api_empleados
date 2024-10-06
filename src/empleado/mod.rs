98be244a942f956c9211d7387577d9367239345a
sergio.israel.hernandez.flores hotmail.com <sergio.israel.hernandez.flores@hotmail.com>
2024-10-05 22:41:37 -0600

    se corrige metodo de prueba

--- src/empleado/mod.rs
+++ src/empleado/mod.rs
@@ -90,5 +90,5 @@ async fn addempleado(empleado: web::Json<EmpleadoDto>) -> impl Responder {
 
 #[post("/api/empleado/hola")]
 async fn hola() -> impl Responder{
-    HttpResponse::body("Hola desde nuevo metodo")
+    HttpResponse::Ok().body("metodo corregido")
 }
