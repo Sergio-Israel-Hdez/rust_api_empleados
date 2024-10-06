92c72b9385d403fd292d8bc41f1fb3e536c52c43
sergio.israel.hernandez.flores hotmail.com <sergio.israel.hernandez.flores@hotmail.com>
2024-10-05 23:28:20 -0600

    se corrige docker compose parase modifica el body del endpoint hola de empleado

--- src/empleado/mod.rs
+++ src/empleado/mod.rs
@@ -90,5 +90,5 @@ async fn addempleado(empleado: web::Json<EmpleadoDto>) -> impl Responder {
 
 #[get("/api/empleado/hola")]
 async fn hola() -> impl Responder{
-    HttpResponse::Ok().body("metodo corregido v2")
+    HttpResponse::Ok().body("metodo corregido v3")
 }
